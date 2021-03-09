bcec9602e1305b6e801b1233dcab4c8b03dba336
ThinkingThigh <thinkingthigh@gmail.com>
2021-03-09 17:39:33 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ThighVueCli
+# ThighCLi
+
+创建模板项目命令行工具
+
+0. 首先确认网络正常且拥有模板项目 git 权限
+1. (sudo) npm install thigh-cli -g 全局安装
+2. thigh-cli init 创建项目
+3. thigh-cli --version 查看命令行版本
---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const inquirer = require("inquirer");
+const shell = require("shelljs");
+const initAction = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "请输入项目名称:",
+        name: "name",
+        validate: (val) => {
+          return val ? true : "请输入项目名称";
+        },
+      },
+      {
+        type: "list",
+        message: "请选择项目类型:",
+        name: "type",
+        choices: ["vue-pc", "vue-mobile", "react-pc", "react-mobile"],
+      },
+    ])
+    .then((answer) => {
+      console.log(`项目 ${answer.name} 生成中... 请稍后..."`);
+      const remote = `git@github.com:ThinkingThigh/${answer.type}.git`;
+      const curName = answer.type;
+      const tarName = answer.name;
+      if (!shell.exec(`git clone ${remote} --depth=1`).code) {
+        shell.mv(curName, tarName);
+        shell.rm("-rf", `./${tarName}/.git`);
+        shell.cd(tarName);
+        if (!shell.exec("npm i").code) {
+          console.log("项目生成成功！");
+        } else {
+          console.log("npm安装失败！");
+        }
+      } else {
+        console.log("模板项目拉取失败！");
+      }
+    });
+};
+
+program.version(require(`./package.json`).version);
+program.command("init").description("生成项目").action(initAction);
+program.parse(process.argv);
